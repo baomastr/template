bbe211819ab9f28b0b65f1109a1a36daa092b767
BaoTheMastr <smashsux@gmail.com>
2017-08-20 13:14:14 +0300

    select styled, radio button styled

--- dist/js/main-debug.js
+++ dist/js/main-debug.js
@@ -39,15 +39,67 @@ $(document).ready(function () {
 
 
     /*чекбокс "пол"*/
-    $('.male').click(function () {
-        $('#check-sex').prop("checked", !$('#check-sex').prop("checked"));
-    });
-
-    $('.female').click(function () {
-        $('#check-sex').trigger('click');
-    });
+    // $('.male').click(function () {
+    //     $('#check-sex').prop("checked", !$('#check-sex').prop("checked"));
+    // });
+    //
+    // $('.female').click(function () {
+    //     $('#check-sex').trigger('click');
+    // });
 
     // $('#check-sex').is(":checked"));
+
+
+    /*
+     custom select
+     */
+
+    $('select').each(function(){
+        var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+        $this.addClass('select-hidden');
+        $this.wrap('<div class="select form__input form__select"></div>');
+        $this.after('<div class="select-styled"></div>');
+
+        var $styledSelect = $this.next('div.select-styled');
+        $styledSelect.text($this.children('option').eq(0).text());
+
+        var $list = $('<ul />', {
+            'class': 'select-options'
+        }).insertAfter($styledSelect);
+
+        for (var i = 0; i < numberOfOptions; i++) {
+            $('<li />', {
+                text: $this.children('option').eq(i).text(),
+                rel: $this.children('option').eq(i).val()
+            }).appendTo($list);
+        }
+
+        var $listItems = $list.children('li');
+
+        $styledSelect.click(function(e) {
+            e.stopPropagation();
+            $('div.select-styled.active').not(this).each(function(){
+                $(this).removeClass('active').next('ul.select-options').hide();
+            });
+            $(this).toggleClass('active').next('ul.select-options').toggle();
+        });
+
+        $listItems.click(function(e) {
+            e.stopPropagation();
+            $styledSelect.text($(this).text()).removeClass('active');
+            $this.val($(this).attr('rel'));
+            $list.hide();
+            //console.log($this.val());
+        });
+
+        $(document).click(function() {
+            $styledSelect.removeClass('active');
+            $list.hide();
+        });
+
+    });
+
 
     console.log($('input[type="file"]').value);
 
